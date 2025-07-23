58b070470ecad9b3449b1904f08c7ad1e77e7027
Matheus Bolina <129126047+mtbolina@users.noreply.github.com>
2025-07-23 11:11:31 -0300

    Update and rename ContadorVogais.py to ManipulandoStrings.py

---
+++ ManipulandoStrings.py
@@ -0,0 +1,7 @@
+NomeCompleto=input("Digite seu nome completo: ")
+
+print(NomeCompleto.upper())
+print(NomeCompleto.lower())
+print(len(NomeCompleto.strip()))
+dividido=NomeCompleto.split()
+print(len(dividido[0]))
